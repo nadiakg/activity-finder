7fbdc170fe80a6cb0478dd169c9e1658ca804cfa
NadiaKG <apple.com>
2015-06-09 14:00:26 -0700

    Add log in functionality, add user authentication, add flash messages, add log out functionality, add remembering upon log in functionality, style log in and log out pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
-	root 'welcome#index'
+  get 'sessions/new'
+
+ 	root 'welcome#index'
 
 	get 'signup'  => 'users#new'
+	get 'login' => 'sessions#new'
+	post 'login' => 'sessions#create'
+	delete 'logout' => 'sessions#destroy'
+
   	resources :users
+
 end
