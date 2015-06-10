503d352a518685561f2e8728e469ea1b73228467
NadiaKG <apple.com>
2015-06-10 13:20:10 -0700

    Add edit form for users, add error messages for unsuccessful edits, add faker and pagination, add admin rights to delete users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(name:  "NadiaKG",
+             email: "nadiakg@afa.com",
+             password:              "foofoo",
+             password_confirmation: "foofoo",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "user-#{n+1}@afa.com"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
