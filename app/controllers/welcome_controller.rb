f0c2baa4e36417d0f99108dda0fae34a30d9719b
NadiaKG <apple.com>
2015-10-12 18:54:39 -0700

    Implement post feed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
   
   def index
+  	if logged_in?
+      @post  = current_user.posts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_many :posts, dependent: :destroy
 	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save :downcase_email
 	before_create :create_activation_digest
@@ -60,6 +61,10 @@ class User < ActiveRecord::Base
 
 	def password_reset_expired?
 		reset_sent_at < 2.hours.ago
+	end
+
+	def feed
+		Post.where("user_id = ?", id)
 	end
 
 	private
